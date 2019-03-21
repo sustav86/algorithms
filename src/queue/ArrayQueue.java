d12e3ff8ce4c012fe238d12a0a28b43b94160582
Sustavov Anton <Anton.Sustavov@alfabank.kiev.ua>
2019-03-21 11:10:29 +0200

    add circular queue

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -15,12 +15,22 @@ public class ArrayQueue {
     }
 
     public void add(Employee employee) {
-        if (back == queue.length) {
+        if (size() == queue.length - 1) {
+            int numItems = size();
             Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
             queue = newArray;
+            front = 0;
+            back = numItems;
         }
-        queue[back++] = employee;
+
+        queue[back] = employee;
+        if (back < queue.length - 1) {
+            back++;
+        }else {
+            back = 0;
+        }
     }
 
     public Employee remove() {
@@ -34,6 +44,8 @@ public class ArrayQueue {
         if (size() == 0) {
             front = 0;
             back = 0;
+        }else if (front == queue.length) {
+            front = 0;
         }
 
         return removeEmployee;
@@ -48,12 +60,22 @@ public class ArrayQueue {
     }
 
     public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        }else {
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 
     public int size() {
-        return back - front;
+        if (front <= back) {
+            return back - front;
+        }else {
+            return back - front + queue.length;
+        }
     }
 }
